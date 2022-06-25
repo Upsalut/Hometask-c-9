b1a3b91e2458310666475938e48fab66539ef4fa
Upper <goodp@yandex.ru>
2022-06-25 18:14:25 +0300

    done

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void NumbersFromMinToMax(int min, int max)
+{
+    if (max < min)
+    {
+        Console.WriteLine();
+        return;
+    }
+    Console.Write(min + " ");
+   NumbersFromMinToMax(min + 1, max);
+}
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+if (m <= 0 || n <= 0)
+    Console.WriteLine("Введены не натуральные числа!");
+else if (m > n)
+{
+    Console.WriteLine("Число M больше числа N, меняем их местами");
+    int tmp = m; m = n; n = tmp;
+    NumbersFromMinToMax(m, n);
+}
+else NumbersFromMinToMax(m, n);
---
+++ Exercise 2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+int AllSum(int min, int max)
+{
+    int sum = min;
+    if (max == min)
+        return sum;
+    return(sum + AllSum(min + 1, max));
+}
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+if (m > n)
+{
+    Console.WriteLine("Число M больше числа N, меняем их местами");
+    int tmp = m; m = n; n = tmp;
+}
+Console.WriteLine($"Сумма чисел от {m} до {n} = {AllSum(m, n)}");
